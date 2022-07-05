d0a6783bbc1dd14732469d8118e01d8141c3188a
schokotets <schokotets@gmail.com>
2022-07-05 22:57:10 +0200

    add elm-websocket-client

--- elm-srcs.nix
+++ elm-srcs.nix
@@ -1,5 +1,10 @@
 {
 
+      "billstclair/elm-websocket-client" = {
+        sha256 = "1402rs1zlpf2aiw4z5d3cm7k3vi5jx7iw1qzb5n86x7m9yar2bm0";
+        version = "4.1.0";
+      };
+
       "elm/html" = {
         sha256 = "1n3gpzmpqqdsldys4ipgyl1zacn0kbpc3g4v3hdpiyfjlgh8bf3k";
         version = "1.0.0";
@@ -20,14 +25,34 @@
         version = "1.1.3";
       };
 
+      "Janiczek/cmd-extra" = {
+        sha256 = "11yh77y4v7dsx7bkhqyx4x0f2hxfz214ydmsp89amy8cnqrkbn5q";
+        version = "1.1.0";
+      };
+
+      "billstclair/elm-port-funnel" = {
+        sha256 = "1l7yjq9wl5gdhvhlig3qk2w4z4hlf898gml7b105icajisysii00";
+        version = "1.2.0";
+      };
+
       "elm/url" = {
         sha256 = "0av8x5syid40sgpl5vd7pry2rq0q4pga28b4yykn9gd9v12rs3l4";
         version = "1.0.0";
       };
 
+      "elm-community/list-extra" = {
+        sha256 = "043iwpdwyrfb1jndxh7kf7xlkgq1v6pjczv1r1b19p2wky6nmy8z";
+        version = "8.5.2";
+      };
+
       "elm/time" = {
         sha256 = "0vch7i86vn0x8b850w1p69vplll1bnbkp8s383z7pinyg94cm2z1";
         version = "1.0.0";
+      };
+
+      "NoRedInk/elm-json-decode-pipeline" = {
+        sha256 = "1k241pjz1wj5rqv95f1j86msa0s0p6w4v8n0jn26aw6cbadw363b";
+        version = "1.0.1";
       };
 
       "elm/virtual-dom" = {
